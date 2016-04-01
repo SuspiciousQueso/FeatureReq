87224bad0e8cc9eab025476a7e09b6338247c551
Billy R Baldwin <billyraybaldwin@gmail.com>
2016-03-31 21:10:40 -0500

    commit new directories for AWS pull

---
+++ model/ConnClass.php
@@ -0,0 +1,13 @@
+<!--
+@Author: Billy R Baldwin <bbaldwin>
+@Date:   03-31-2016
+@Email:  billyraybaldwin@gmail.com
+@Project: FeatureREQ
+@Last modified by:   bbaldwin
+@Last modified time: 03-31-2016
+-->
+<?php
+
+
+
+?>
