d80839b09ce47f105d96858df31a9f50049aa513
Billy R Baldwin <billyraybaldwin@gmail.com>
2016-03-31 20:19:53 -0500

    pushing local source

---
+++ cont/global_vars.php
@@ -0,0 +1,15 @@
+<!--
+@Author: Billy R Baldwin <bbaldwin>
+@Date:   03-31-2016
+@Email:  billyraybaldwin@gmail.com
+@Project: FeatureREQ
+@Last modified by:   bbaldwin
+@Last modified time: 03-31-2016
+-->
+<?php
+// Username and password for your database
+$un = 'bbaldwin';
+$pw = '#iG00gl311#';
+
+
+?>
---
+++ view/index.php
@@ -0,0 +1,14 @@
+<!--
+@Author: Billy R Baldwin <bbaldwin>
+@Date:   03-31-2016
+@Email:  billyraybaldwin@gmail.com
+@Project: FeatureREQ
+@Last modified by:   bbaldwin
+@Last modified time: 03-31-2016
+-->
+<?php
+
+echo "hiya!";
+
+
+ ?>
